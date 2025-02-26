10d7574a527155f4c44e34000c96570a70e27995
BitanXD <bitansarkar088@gmail.com>
2025-02-27 01:26:30 +0530

    add function and edit function done

--- script.js
+++ script.js
@@ -18,4 +18,22 @@ function addTask() {
 `;
 
   listContainer.appendChild(li);
+  inputBox.value = ""
+
+  const checkBox = li.querySelector("input")
+  const editBtn = li.querySelector(".edit-btn")
+  const deleteBtn = li.querySelector(".delete-btn")
+  const taskSpan = li.querySelector("span")
+
+  checkBox.addEventListener("click", function() {
+    li.classList.toggle("completed", checkBox.checked)
+  })
+
+  editBtn.addEventListener("click", function() {
+    const update = prompt("Edit task:", taskSpan.textContent)
+    if(update !== null){
+        taskSpan.textContent = update;
+        li.classList.remove("completed")
+    }
+  })
 }

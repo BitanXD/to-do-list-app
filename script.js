3c6e9f4d72f30103f9f0bf769bd1ab6a4c0d811f
BitanXD <bitansarkar088@gmail.com>
2025-02-27 16:29:21 +0530

    added counter and delete function, also done with css

--- script.js
+++ script.js
@@ -18,22 +18,48 @@ function addTask() {
 `;
 
   listContainer.appendChild(li);
-  inputBox.value = ""
+  inputBox.value = "";
 
-  const checkBox = li.querySelector("input")
-  const editBtn = li.querySelector(".edit-btn")
-  const deleteBtn = li.querySelector(".delete-btn")
-  const taskSpan = li.querySelector("span")
+  const checkBox = li.querySelector("input");
+  const editBtn = li.querySelector(".edit-btn");
+  const deleteBtn = li.querySelector(".delete-btn");
+  const taskSpan = li.querySelector("span");
 
-  checkBox.addEventListener("click", function() {
-    li.classList.toggle("completed", checkBox.checked)
-  })
+  // this will toggle checkbox
+  checkBox.addEventListener("click", function () {
+    li.classList.toggle("completed", checkBox.checked);
+    //update counters when checkbox is clicked for a task
+    updateCounters();
+  });
 
-  editBtn.addEventListener("click", function() {
-    const update = prompt("Edit task:", taskSpan.textContent)
-    if(update !== null){
-        taskSpan.textContent = update;
-        li.classList.remove("completed")
+  // this will enable edit button and remove the completed functionality
+  editBtn.addEventListener("click", function () {
+    const update = prompt("Edit task:", taskSpan.textContent);
+    if (update !== null) {
+      taskSpan.textContent = update;
+      li.classList.remove("completed");
+      //set checkbox to false when you edit a task and update the counter as well
+      checkBox.checked = false;
+      updateCounters();
+    }
+  });
+
+  const completedCounter = document.getElementById("completed-counter")
+  const notCompletedCounter = document.getElementById("not-completed-counter")
+  //function to update counters
+  function updateCounters() {
+    const completedTasks = document.querySelectorAll(".completed").length;
+    const notCompletedTasks = document.querySelectorAll("li:not(.completed)").length;
+    completedCounter.textContent = completedTasks;
+    notCompletedCounter.textContent = notCompletedTasks;
+  }
+  updateCounters();
+
+  // delete a task
+  deleteBtn.addEventListener("click", function() {
+    if(confirm("Do you want to delete this task?")){
+      li.remove();
+      updateCounters();
     }
   })
 }
